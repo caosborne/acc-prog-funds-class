a0b6e06b3a4db93a38840facbbcadfe43533ab13
Christopher Osborne <chris.a.osborne@gmail.com>
2017-02-21 13:09:41 -0600

    completed module 2a and almost with 2b

--- competency-module-2/christopher_osborne_labprojectmodule2a.py
+++ competency-module-2/christopher_osborne_labprojectmodule2a.py
@@ -17,71 +17,74 @@ print('     Rumble     Rumble     Rumble')
 print('I think we just felt an earthquake!')
 print(' ')
 
+# if richterScale < 1 or richterScale > 10:
+#     i = 0
 # Need a loop before richterScale to come back to in the first if statement after they user is promtped that answer is incorrect.
-
-# Set variable to an input asking the user how big they think that earthquake was on a richter scale
-richterScale = float(input('How big do you think that was on the Richter Scale? (Usually a Richter Scale is measured from 1-10)'))
-print(' ')
-print('=========================================================')
-print(' ')
-
-# Need to set a parameter of the input not being less than 1 or greater than 10
-# User will get three chances to input a correct number if not the program aborts
-    # This will need to be a loop that checks if number was input correctly.
-    # If not ask again
-if richterScale < 1 or richterScale > 10:
-    i = 0
-    print('That is not a valid Richter Scale measurement.')
-    while i < 3:
-        i += 1
-        if i < 3:
-            print('Please try putting in a correct Richter Scale value.')
-            # richterScale = float(input('How big do you think that was on the Richter Scale? (Usually a Richter Scale is measured from 1-10)'))
-        else:
-            print('You have entered an incorrect number 3 times. Goodbye!')
-else:
-    # There needs to be parameters with prints that same some comment
-        # 1-4 "You call that an earthquake?"
-        # 5-8 "That's not a earthquake"
-        # 9-10 "Now that's an earthquake!"
-    if richterScale <= 4:
-        print('You call that an earthquake?')
-        print('  ')
-    elif richterScale > 4 and richterScale <= 8:
-        print('That\'s not a earthquake!')
-        print('  ')
-    elif richterScale > 8 and richterScale <= 10:
-        print('Now that\'s an earthquake!')
-        print('  ')
-    # print the value the user input and then run the measurements
-    print('A Richter Scale value of ', richterScale, ' results in the following: ')
-
-    # create a variable with the equation of energy(joules) and explodedTNT
-        # Energy = 10^(1.5*richter)+4.8
-        # 1 joule = 2.390057361377 * 10-7 kilogram of TNT
-        # To raise a number to a power, use the ** operator. For example:
-        # print( "The number 10 raised to the power 1.5 is", 10**1.5 )
-    energy = 10 ** (( 1.5 * richterScale ) + 4.8 )
-    explodedTNT = (energy * (2.390057361377 * 10 ** -7)) / 1000
-
-    # print the results from the variables set of the equations for energy(joules) and explodedTNT
-    # check these outputs with the website given in instructions to confirm they're correct
-    print('     In joules: ', energy)
-    print('     Tons of TNT exploded: ', explodedTNT)
-
+counter = 0
+while counter < 3:
+    # Set variable to an input asking the user how big they think that earthquake was on a richter scale
+    richterScale = float(input('How big do you think that was on the Richter Scale? (Usually a Richter Scale is measured from 1-10) '))
     print(' ')
     print('=========================================================')
     print(' ')
+    # i += 1
+    # Need to set a parameter of the input not being less than 1 or greater than 10
+    # User will get three chances to input a correct number if not the program aborts
+        # This will need to be a loop that checks if number was input correctly.
+        # If not ask again
+    if richterScale < 1 or richterScale > 10:
+        print('That is not a valid Richter Scale measurement.')
+        print('Please try putting in a correct Richter Scale value.')
+        print('  ')
+        print('  ')
+        # richterScale = float(input('How big do you think that was on the Richter Scale? (Usually a Richter Scale is measured from 1-10)'))
+        counter += 1
+    else:
+        # There needs to be parameters with prints that same some comment
+            # 1-4 "You call that an earthquake?"
+            # 5-8 "That's not a earthquake"
+            # 9-10 "Now that's an earthquake!"
+        if richterScale <= 4:
+            print('You call that an earthquake?')
+            print('  ')
+        elif richterScale > 4 and richterScale <= 8:
+            print('That\'s not a earthquake!')
+            print('  ')
+        elif richterScale > 8 and richterScale <= 10:
+            print('Now that\'s an earthquake!')
+            print('  ')
+        # print the value the user input and then run the measurements
+        print('A Richter Scale value of ', richterScale, ' results in the following: ')
 
-    # print the same message from above letting the user know what richter scale measuremen they chose and now let them know this will be in scientific notation
-    print('A Richter Scale value of ', richterScale, ' results in the following fancy scientific notation: ')
+        # create a variable with the equation of energy(joules) and explodedTNT
+            # Energy = 10^(1.5*richter)+4.8
+            # 1 joule = 2.390057361377 * 10-7 kilogram of TNT
+            # To raise a number to a power, use the ** operator. For example:
+            # print( "The number 10 raised to the power 1.5 is", 10**1.5 )
+        energy = 10 ** (( 1.5 * richterScale ) + 4.8 )
+        explodedTNT = (energy * (2.390057361377 * 10 ** -7)) / 1000
 
-    # set variables for the energy and explodedTNT variables but in scientific notation
-        # To indicate scientific notation in Python, use the format specifier.
-        # Example: print(format(354813389233576.06, ‘.2e’))
-    energySN = format(energy, '.2e')
-    explodedTNT_SN = format(explodedTNT, '.2e')
+        # print the results from the variables set of the equations for energy(joules) and explodedTNT
+        # check these outputs with the website given in instructions to confirm they're correct
+        print('     In joules: ', energy)
+        print('     Tons of TNT exploded: ', explodedTNT)
 
-    # print the results from the scientific notation and confirm they're correct with same website as before.
-    print('     In joules: ', energySN)
-    print('     Tons of TNT exploded: ', explodedTNT_SN)
+        print(' ')
+        print('=========================================================')
+        print(' ')
+
+        # print the same message from above letting the user know what richter scale measuremen they chose and now let them know this will be in scientific notation
+        print('A Richter Scale value of ', richterScale, ' results in the following fancy scientific notation: ')
+
+        # set variables for the energy and explodedTNT variables but in scientific notation
+            # To indicate scientific notation in Python, use the format specifier.
+            # Example: print(format(354813389233576.06, ‘.2e’))
+        energySN = format(energy, '.2e')
+        explodedTNT_SN = format(explodedTNT, '.2e')
+
+        # print the results from the scientific notation and confirm they're correct with same website as before.
+        print('     In joules: ', energySN)
+        print('     Tons of TNT exploded: ', explodedTNT_SN)
+        break
+else:
+    print('Clearly the earthquake rattled your brain and you have entered an incorrect Richter Scale value 3 times. Goodbye!')
--- competency-module-2/christopher_osborne_labprojectmodule2b.py
+++ competency-module-2/christopher_osborne_labprojectmodule2b.py
@@ -5,10 +5,20 @@
 #         i. To do this you will use a selection within the loop. You will NOT have seven unique input statements. It will be one statement repeated 7 times with the loop iterations
 eggsTotal = 0
 weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+# 3. Ensure that Farmer Suzy does not enter any value less than 55 or greater than
+# 550 for the number of eggs collected each day
+# a. This requirement is based on the assumption that each chicken lays at
+# least one egg per day and no chicken lays more than 10 eggs per day
 for i in range(len(weekdays)):
     # print(weekdays[i])
     dailyEggs = int(input('How many eggs did you collect on ' + weekdays[i] + '?'))
-    eggsTotal += dailyEggs
+    if dailyEggs < 55 or dailyEggs > 550:
+        # do something
+        print('There needs to be more than 55 and less 550 eggs per day!')
+        continue
+    else:
+        eggsTotal += dailyEggs
+# 2. You will input the road Farmer Suzy will take to the Farmer’s Market.
 route = input('What route would you like to take today? Enter M for Mountain or R for River.')
 routeMiles = 0
 if route == 'm':
@@ -16,29 +26,35 @@ if route == 'm':
 elif route == 'r':
     routeMiles += 12.22
 # print(routeMiles)
+# a. The total eggs collected during the week
 print('         Total eggs collected                ', eggsTotal)
+# b. The number of cartons Farmer Suzy will cart to the Farmer’s Market
 cartons = eggsTotal // 12
 print('         Total cartons made                  ', cartons)
 
-# 2. You will input the road Farmer Suzy will take to the Farmer’s Market.
-# 3. Ensure that Farmer Suzy does not enter any value less than 55 or greater than
-# 550 for the number of eggs collected each day
-# a. This requirement is based on the assumption that each chicken lays at
-# least one egg per day and no chicken lays more than 10 eggs per day
-# 4. Calculate:
-# a. The total eggs collected during the week
-# b. The number of cartons Farmer Suzy will cart to the Farmer’s Market
+# cartonSize = ['medium', 'large', 'jumbo', 'uber']
+mediumCarton = int(input('How many medium cartons did you sell: '))
+largeCarton = int(input('How many large cartons did you sell: '))
+jumboCarton = int(input('How many jumbo cartons did you sell: '))
+uberCarton = int(input('How many uber cartons did you sell: '))
+
+# Does the number of cartons sold need to equal the cartons I took to the market?
+#  If so I need to set a check to make sure the numbers sold match the numbers taken to the market
+
+print('  ')
+print('====================================================================================')
+print('  ')
+# $4 per dozen for medium eggs
+# $5 per dozen for large eggs
+# $6 per dozen for jumbo eggs
+# $7 per dozen for uber eggs
 # c. The gross proceeds from the sales of eggs – remember there are four
 # different rate levels (see page 1)
-# d. The total expenditures Farmer Suzy has incurred for the eggs including
-# gasoline for the truck
-# e. The weekly profit
-# 5. If all the parameters remain the same each week, can you now forecast the annual profit for Farmer Suzy?
-
-chickenFeed = .10 * 55
-coopUpkeep = .12 * 55
-helper = 8.50
-
+mediumCartonsSold = mediumCarton * 4
+largeCartonsSold = largeCarton * 5
+jumboCartonsSold = jumboCarton * 6
+uberCartonsSold = uberCarton * 7
+totalCartonSales = mediumCartonsSold + largeCartonsSold + jumboCartonsSold + uberCartonsSold
 
 # 1. Farmer Suzy has 55 chickens
 # 2. She spends $0.10 per chicken per week on chicken feed
@@ -56,8 +72,39 @@ helper = 8.50
 # 10. Farmer Suzy’s truck gives an astoundingly high 6.5 MPG
 # 11. Gas costs $3.99 per gallon
 # 12. Net profit takes into account all revenues and all expenditures
+# d. The total expenditures Farmer Suzy has incurred for the eggs including
+# gasoline for the truck
+weeklyChickenFeed = .10 * 55
+weeklyCoopUpkeep = .12 * 55
+weeklyTotalChickenMaintanence = weeklyCoopUpkeep + weeklyChickenFeed
+weeklyHelperWage = 8.50 * 15
+commission = totalCartonSales * .01
+# milesRoundtrip = routeMiles * 2
+# gallonsUsed = float(milesRoundtrip / 6.5)
+# gasolineCost = gallonsUsed * 3.99
+# print('  ')
+# print(milesRoundtrip)
+# print(format(gallonsUsed, '.2f'))
+# print(gasolineCost)
+# print('  ')
+gasolineUsed = (( routeMiles * 2 ) / 6.5) * 3.99
+totalExpenditure = commission + weeklyHelperWage + weeklyTotalChickenMaintanence + gasolineUsed
+weeklyProfit = totalCartonSales - totalExpenditure
+projectedAnnualProfit = weeklyProfit * 52
+# e. The weekly profit
+# 5. If all the parameters remain the same each week, can you now forecast the annual profit for Farmer Suzy?
 
-# $4 per dozen for medium eggs
-# $5 per dozen for large eggs
-# $6 per dozen for jumbo eggs
-# $7 per dozen for uber eggs
+print('For the week this is what your financials look like:')
+print('         Total eggs collected                ', eggsTotal)
+print('         Total cartons made                  ', cartons)
+print('Total Expenses:')
+print('         Helper Salary                       ', format(weeklyHelperWage, '.2f'))
+print('         Market Fee                          ', format(commission, '.2f'))
+print('         Chicken Maintenance                 ', format(weeklyTotalChickenMaintanence, '.2f'))
+print('         Gasoline Used                       ', format(gasolineUsed, '.2f'))
+print('         Total Expenditure                   ', format(totalExpenditure, '.2f'))
+print('Total Revenue:')
+print('         Egg Sales                           ', format(totalCartonSales, '.2f'))
+print('Profits:')
+print('         Weekly Profit                       ', format(weeklyProfit, '.2f'))
+print('         Projected Annual Profit             ', format(projectedAnnualProfit, '.2f'))
